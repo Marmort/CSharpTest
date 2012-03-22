161f652aad1b8b825b6ff29a6631336e262268e2
Henry Yang <yangbo.game@gmail.com>
2012-03-22 20:26:48 +0800

    Hello World
    Signed-off-by: Henry Yang <yangbo.game@gmail.com>

---
+++ ProtoBuf/ProtoBuf/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProtocolBuffers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.Read();
+        }
+    }
+}
