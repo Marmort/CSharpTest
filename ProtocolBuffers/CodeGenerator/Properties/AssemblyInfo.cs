3538db01c756716b3959fac53658e36d681efd21
Henry Yang <yangbo.game@gmail.com>
2012-03-25 11:05:20 +0800

    移动文件和目录

--- ProtocolBuffers/CodeGenerator/Properties/AssemblyInfo.cs
+++ ProtocolBuffers/CodeGenerator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ProtoBuf")]
+[assembly: AssemblyTitle("CodeGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProtoBuf")]
+[assembly: AssemblyProduct("CodeGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("b153b1af-877c-4fd1-9ee5-64ed4c7dfd80")]
+[assembly: Guid("a366640c-6d2e-480f-a037-450909a816ce")]
 
 // 程序集的版本信息由下面四个值组成:
 //
