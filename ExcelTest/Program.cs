c6d5ce3e9deb2f41b2648653839a8c7ba4ba1239
Advansil <marmortor@gmail.com>
2012-03-29 16:42:18 +0800

    ExcelTest

---
+++ ExcelTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelTest
+{
+    class Program
+    {
+        // 全局变量
+        public static String programPath = System.IO.Directory.GetCurrentDirectory();
+
+        static void Main(string[] args)
+        {
+            InitializeExcel.InstallExcel();            
+            string names = programPath + "\\aaa.xlsx";
+            Console.WriteLine(names);
+            InitializeExcel.OpenExcel(names);
+            Console.ReadKey();
+        }
+    }
+}
